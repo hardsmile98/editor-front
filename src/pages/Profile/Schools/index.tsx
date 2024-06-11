94ad2021a06bee1510b65a7e25d9760c18be897d
Денис Котелев <d.kotelev@carprice.ru>
2024-06-11 17:14:07 +0300

    f

--- src/pages/Profile/Schools/index.tsx
+++ src/pages/Profile/Schools/index.tsx
@@ -36,6 +36,192 @@ function Schools() {
             </Box>
           </Paper>
         </Link>
+
+        <Link to="/school/1">
+          <Paper sx={styles.item}>
+            <Box sx={styles.content}>
+              <Avatar
+                alt="Remy Sharp"
+                src="/static/images/avatar/1.jpg"
+                sx={styles.avatar}
+              />
+
+              <Typography>
+                Формула запуска
+              </Typography>
+            </Box>
+
+            <Box sx={styles.students}>
+              <PeopleIcon />
+              0
+            </Box>
+          </Paper>
+        </Link>
+
+        <Link to="/school/1">
+          <Paper sx={styles.item}>
+            <Box sx={styles.content}>
+              <Avatar
+                alt="Remy Sharp"
+                src="/static/images/avatar/1.jpg"
+                sx={styles.avatar}
+              />
+
+              <Typography>
+                Формула запуска
+              </Typography>
+            </Box>
+
+            <Box sx={styles.students}>
+              <PeopleIcon />
+              0
+            </Box>
+          </Paper>
+        </Link>
+
+        <Link to="/school/1">
+          <Paper sx={styles.item}>
+            <Box sx={styles.content}>
+              <Avatar
+                alt="Remy Sharp"
+                src="/static/images/avatar/1.jpg"
+                sx={styles.avatar}
+              />
+
+              <Typography>
+                Формула запуска
+              </Typography>
+            </Box>
+
+            <Box sx={styles.students}>
+              <PeopleIcon />
+              0
+            </Box>
+          </Paper>
+        </Link>
+
+        <Link to="/school/1">
+          <Paper sx={styles.item}>
+            <Box sx={styles.content}>
+              <Avatar
+                alt="Remy Sharp"
+                src="/static/images/avatar/1.jpg"
+                sx={styles.avatar}
+              />
+
+              <Typography>
+                Формула запуска
+              </Typography>
+            </Box>
+
+            <Box sx={styles.students}>
+              <PeopleIcon />
+              0
+            </Box>
+          </Paper>
+        </Link>
+        <Link to="/school/1">
+          <Paper sx={styles.item}>
+            <Box sx={styles.content}>
+              <Avatar
+                alt="Remy Sharp"
+                src="/static/images/avatar/1.jpg"
+                sx={styles.avatar}
+              />
+
+              <Typography>
+                Формула запуска
+              </Typography>
+            </Box>
+
+            <Box sx={styles.students}>
+              <PeopleIcon />
+              0
+            </Box>
+          </Paper>
+        </Link>
+        <Link to="/school/1">
+          <Paper sx={styles.item}>
+            <Box sx={styles.content}>
+              <Avatar
+                alt="Remy Sharp"
+                src="/static/images/avatar/1.jpg"
+                sx={styles.avatar}
+              />
+
+              <Typography>
+                Формула запуска
+              </Typography>
+            </Box>
+
+            <Box sx={styles.students}>
+              <PeopleIcon />
+              0
+            </Box>
+          </Paper>
+        </Link>
+        <Link to="/school/1">
+          <Paper sx={styles.item}>
+            <Box sx={styles.content}>
+              <Avatar
+                alt="Remy Sharp"
+                src="/static/images/avatar/1.jpg"
+                sx={styles.avatar}
+              />
+
+              <Typography>
+                Формула запуска
+              </Typography>
+            </Box>
+
+            <Box sx={styles.students}>
+              <PeopleIcon />
+              0
+            </Box>
+          </Paper>
+        </Link>
+
+        <Link to="/school/1">
+          <Paper sx={styles.item}>
+            <Box sx={styles.content}>
+              <Avatar
+                alt="Remy Sharp"
+                src="/static/images/avatar/1.jpg"
+                sx={styles.avatar}
+              />
+
+              <Typography>
+                Формула запуска
+              </Typography>
+            </Box>
+
+            <Box sx={styles.students}>
+              <PeopleIcon />
+              0
+            </Box>
+          </Paper>
+        </Link>
+
+        <Link to="/school/1">
+          <Paper sx={styles.item}>
+            <Box sx={styles.content}>
+              <Avatar
+                alt="Remy Sharp"
+                src="/static/images/avatar/1.jpg"
+                sx={styles.avatar}
+              />
+
+              <Typography>
+                Формула запуска
+              </Typography>
+            </Box>
+
+            <Box sx={styles.students}>
+              <PeopleIcon />
+              0
+            </Box>
+          </Paper>
+        </Link>
       </Box>
     </>
   );
