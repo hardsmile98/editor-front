0dd123404b51ed2fd09fe0bb08093322450cbdc6
Денис Котелев <d.kotelev@carprice.ru>
2024-06-21 10:49:02 +0300

    f

--- src/components/Preview/Banner/index.tsx
+++ src/components/Preview/Banner/index.tsx
@@ -11,17 +11,47 @@ const styles = {
     pb: '55%',
     backgroundColor: 'background.paper',
     borderRadius: 1,
+    position: 'relative',
+    overflow: 'hidden',
+  },
+
+  title: {
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+    textAlign: 'center',
   },
 };
 
 function Banner({ properties }: BannerProps) {
-  console.log(properties);
+  const title = properties.title?.value;
+
+  const sx = {
+    backgroundColor: String(properties?.backgroundColor?.value),
+    borderStyle: 'solid',
+    borderColor: String(properties?.border?.value),
+    borderWidth: Number(properties?.border?.width),
+    marginTop: `${Number(properties?.margin?.topValue)}px`,
+    marginBottom: `${Number(properties?.margin?.bottomValue)}px`,
+    backgroundImage: properties?.image?.value && `url(${properties?.image?.value})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    pb: properties?.ratio?.value ? '55%' : '100%',
+  };
+
+  const sxTitle = {
+    fontSize: `${Number(properties?.fontSize?.value)}px`,
+  };
 
   return (
-    <Box sx={styles.root}>
-      <Box>
-        12312123
-      </Box>
+    <Box sx={{ ...styles.root, ...sx }}>
+      {title && (
+        <Box sx={{ ...styles.title, ...sxTitle }}>
+          {title}
+        </Box>
+      )}
     </Box>
   );
 }
